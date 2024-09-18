df85ef90e74e1c81b03c553a8e03b7e9790e3da2
Damira <arimad1991@gmail.com>
2024-09-18 14:20:10 +0600

    Homework20 for lesson 43 is ready

--- backend/src/main/java/ru/flamexander/transfer/service/core/backend/configurations/AppConfig.java
+++ backend/src/main/java/ru/flamexander/transfer/service/core/backend/configurations/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import ru.flamexander.transfer.service.core.backend.configurations.RestClientFactory;
 
 @Configuration
@@ -32,5 +33,14 @@ public class AppConfig {
     @ConditionalOnMissingBean(RestTemplate.class)
     public RestClient limitsClient(LimitsServiceProperties properties) {
         return restClientFactory.createRestClient(properties);
+    }
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        return filter;
     }
 }
